be590a248423205507bd188f13992cac0fab373a
Muntaha2427 <sidratm546@gmail.com>
2026-03-24 00:40:14 +0600

    From Python Crash Course

---
+++ Lab Report 3(for loop-ch4)/problem4_1.py
@@ -0,0 +1,5 @@
+pizza_names=['bufala','marinara','diavola','pepperoni','calzone']
+for pizza in pizza_names:
+    print(pizza)
+    print(f"I like {pizza.title()} pizza.")
+print("\nI really love pizza!")
