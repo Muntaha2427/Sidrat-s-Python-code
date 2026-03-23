0856ba4b66ab4f2e7c7c1f98aa7484fb179a7155
Muntaha2427 <sidratm546@gmail.com>
2026-03-24 02:57:17 +0600

    From Python Crash Course

---
+++ Lab Report 3(for loop-ch4)/problem4_2.py
@@ -0,0 +1,5 @@
+animals=['humans','chimpanzees','monkey']
+for animal in animals:
+    print(animal)
+    print(f"The {animal} can eat food by their own.")
+print("\nAll of them belong from Mammalia class.")
