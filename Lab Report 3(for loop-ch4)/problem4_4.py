d23dce3a2908c8f921696001a2da45fe0185bca5
Muntaha2427 <sidratm546@gmail.com>
2026-03-24 02:58:40 +0600

    From Python Crash Course

---
+++ Lab Report 3(for loop-ch4)/problem4_4.py
@@ -0,0 +1,4 @@
+million_numbers=[]
+for numbers in range(1,1000001):
+    million_numbers.append(numbers)
+print(million_numbers)
---
+++ Lab Report 3(for loop-ch4)/problem4_5.py
@@ -0,0 +1,4 @@
+million_numbers=list(range(1,1000001))
+print(f"Minimum Number is {min(million_numbers)}")
+print(f"Maximum Number is {max(million_numbers)}")
+print(f"Summation of Million Numbers is {sum(million_numbers)}")
