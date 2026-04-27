3ddd5548f982ab1feec1bbb4eda5f3903c936eca
Muntaha2427 <sidratm546@gmail.com>
2026-04-27 23:25:46 +0600

    From python Crash Course

---
+++ Lab Report 5(Dictionaries-Ch6)/problem6_2.py
@@ -0,0 +1,9 @@
+favourite_numbers={
+   'Tappu':10,
+   'Sonu':1,
+   'Aladdin':5,
+   'Balveer':8,
+   'Nobita':15,
+}
+for person, numbers in favourite_numbers.items():
+    print(f"{person}'s favourite number is {numbers}.")
