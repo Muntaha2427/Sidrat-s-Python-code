8066ffd8fe86f6db6740eb2d2e81354cf847fd3f
Muntaha2427 <sidratm546@gmail.com>
2026-04-27 21:06:23 +0600

    From Python Crash Course

---
+++ Lab Report 5(Dictionaries-Ch6)/problem6_1.py
@@ -0,0 +1,5 @@
+person_info={'first_name':'Sidratul','last_name':'Muntaha','age':21,'city':'Feni'}
+print("First name is " + person_info['first_name'].title())
+print("Last name is " + person_info['last_name'].title())
+print("Age is " + str(person_info['age']) + " years old")
+print("Living city is " + person_info['city'].title())
