f6e7411178b1554440d3d3c07e4d6f427fd8c968
Muntaha2427 <sidratm546@gmail.com>
2026-05-26 02:05:00 +0600

    From Python Crash Course

---
+++ Lab Report 6(User Input & While Loops)/problem7_2.py
@@ -0,0 +1,7 @@
+message_Q_A=input("How many people are your dinner group?\n")
+message_Q_A=int(message_Q_A)
+if message_Q_A>8:
+    print("Sorry! You will have to wait for a table.")
+else:
+    print("Welcome! Your table is ready.")
+  
